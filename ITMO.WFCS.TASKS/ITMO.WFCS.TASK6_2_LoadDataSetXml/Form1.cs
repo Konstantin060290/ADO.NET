ab553dd2180b4fcc0f8871744624a50442e48db4
Константин Филюрин <rfrsk@yandex.ru>
2021-02-16 21:45:43 +0300

    Добавил лабораторную работу 6

---
+++ ITMO.WFCS.TASKS/ITMO.WFCS.TASK6_2_LoadDataSetXml/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WFCS.TASK6_2_LoadDataSetXml
+{
+    public partial class Form1 : Form
+    {
+
+
+        DataSet NorthwindDataSet = new DataSet();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadSchemaButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXmlSchema("Northwind.xsd");
+            CustomersGrid.DataSource = NorthwindDataSet.Tables["Customers"];
+            OrdersGrid.DataSource = NorthwindDataSet.Tables["Orders"];
+
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXml("Northwind.xml");
+           
+        }
+    }
+}
